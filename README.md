c11efc554168cbadc40637c55594607c6c60f56e
Gustavo <gustavo.gofran@gmail.com>
2024-03-04 23:44:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tempo-temp
+ A simple app to check the weather and temperature of all cities in the world.
---
+++ tempo-temp/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  tempo-temp
+//
+//  Created by Gustavo Francischini on 04/03/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "corPrimaria")
+    static let softGrayX = UIColor(named: "softGray")
+}
